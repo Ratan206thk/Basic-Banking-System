571868f003e18daefa42a4d6167097fa21f5fe45
Ratan206thk <Ratan206thk@gmail.com>
2021-07-09 01:40:45 +0530

    'update'

--- routes/router.js
+++ routes/router.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const bodyParser = require('body-parser');
 const MongoClient = require('mongodb').MongoClient;
-const url = "mongodb://localhost:27017/";
+const url = "mongodb+srv://user:<ripFasicHAqzBkEU>@sparks.jcwap.mongodb.net/user?retryWrites=true&w=majority";
 module.exports = () => {
   router.use(bodyParser.urlencoded());
   router.get('/', (request, response) => {
